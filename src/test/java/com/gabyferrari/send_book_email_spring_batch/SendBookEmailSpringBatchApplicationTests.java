5ac3143d5bafe2b2d49ab1251dc32af017c805d3
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-17 12:36:38 -0300

    Project created

---
+++ src/test/java/com/gabyferrari/send_book_email_spring_batch/SendBookEmailSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.send_book_email_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendBookEmailSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
