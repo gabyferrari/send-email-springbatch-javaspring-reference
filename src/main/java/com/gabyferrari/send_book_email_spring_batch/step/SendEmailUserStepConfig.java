46944821aa900de45ccbedebc4b1c7ddd7498457
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-17 14:39:28 -0300

    Job and Step basic configuration

---
+++ src/main/java/com/gabyferrari/send_book_email_spring_batch/step/SendEmailUserStepConfig.java
@@ -0,0 +1,29 @@
+package com.gabyferrari.send_book_email_spring_batch.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.gabyferrari.send_book_email_spring_batch.domain.UserBookLoan;
+
+@Configuration
+public class SendEmailUserStepConfig {
+	
+	@Autowired
+	@Qualifier("transactionManagerApp")
+	private PlatformTransactionManager transactionManager;
+
+	@Bean
+	Step sendEmailUserStep(ItemReader<UserBookLoan> readUsersWithLoansCloseToReturnReader, JobRepository jobRepository) {
+		return new StepBuilder("sendEmailUserStep", jobRepository)
+				.<UserBookLoan, UserBookLoan>chunk(1, transactionManager)
+				.reader(readUsersWithLoansCloseToReturnReader)
+				.build();
+	}
+}
